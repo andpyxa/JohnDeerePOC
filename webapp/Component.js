3cd37a6c1e1db6bc00cf54f6c9d5991291a82a02
Saswata Chandra <saswatapablo@gmail.com>
2020-02-21 09:31:01 +0000

    Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("com.deere.zui5_flight_booking.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
